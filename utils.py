93912d7dd2d87a236c897a524bf61173120c95ed
Teut2711 <manglavishesh64@gmail.com>
2023-08-28 00:23:15 +0530

    feat: created function to convert text to kebab case

--- utils.py
+++ utils.py
@@ -16,3 +16,13 @@ def get_district_name(input_string):
 
 def kebab_to_title(kebab_case_string):
     return kebab_case_string.replace("-", " ").title()
+
+
+def space_to_lowercase_kebab_case(input_string):
+    # Split the input string by spaces and convert to lowercase
+    words = input_string.lower().split()
+
+    # Join the words with hyphens to create lowercase kebab case
+    kebab_case = '-'.join(words)
+
+    return kebab_case
