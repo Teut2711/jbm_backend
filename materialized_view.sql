7d1be3f25164bbdaa2759bd4c991ba39b7feeac0
Teut2711 <manglavishesh64@gmail.com>
2023-09-22 12:57:39 +0530

    fix: changed index creation queries for new columns in faults

--- materialized_view.sql
+++ materialized_view.sql
@@ -1,3 +1,3 @@
 CREATE UNIQUE INDEX unique_imei_index ON bus_battery_data ("imei");
-CREATE UNIQUE INDEX unique_fault_index ON bus_faults_data ("imei", "fault_description", "fault_time");
+CREATE UNIQUE INDEX unique_fault_index ON bus_faults_data ("imei", "fault_description", "start_time");
 
